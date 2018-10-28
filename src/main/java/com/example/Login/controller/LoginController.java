cdc4c6ba552669015721b88a391e8818f231e355
Dilshanth <dilshanth10@gmail.com>
2018-10-28 21:04:13 +0530

    cross origin

--- src/main/java/com/example/Login/controller/LoginController.java
+++ src/main/java/com/example/Login/controller/LoginController.java
@@ -1,8 +1,8 @@
 package com.example.Login.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,10 +10,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.example.Login.entity.Login;
 import com.example.Login.service.LoginService;
 
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
 public class LoginController {
 	@Autowired
